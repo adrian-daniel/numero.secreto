7ef5758a3754ac16d9dc25079c694743025c19d7
Adrian Daniel <adrian.daniel.136@ufrn.edu.br>
2025-02-26 19:17:47 -0300

    Reverter 'remover título'

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número oculto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e ' + numeroLimite);
 }
 
